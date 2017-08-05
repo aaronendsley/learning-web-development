82d3353503c33ca26f1281f6182157c5bbc327f1
Aaron Endsley <aaronendsley@gmail.com>
2017-08-04 20:39:18 -0500

    added the variables folder and other stater files

---
+++ variables/variables.js
@@ -0,0 +1,19 @@
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+
+Today your going to write your first variable and store data.
+
+If you get confused at any time above is a link to the MDN doc on Javascript Variables
+*/
+
+// Declare a variable
+// Refer to the link above
+
+// Store a string in a variable
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+//Store a Number in a variable
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+
+// Store a Boolean value in a variable
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
