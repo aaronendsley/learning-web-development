d91e73fd5d6721eb76611a6d6d849b131567a860
Cr1msonFir3 <eternalblad3@gmail.com>
2017-08-05 10:40:37 -0500

    added solutions to the problems

--- variables/variables-solution.js
+++ variables/variables-solution.js
@@ -1,7 +1,9 @@
 /*
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
 
-Today your going to write your first variable and store data.
+Today your going to write your first variable and store data. 
+
+Any variable and and any value can be used as long as the format is correct.
 
 If you get confused at any time above is a link to the MDN doc on Javascript Variables
 */
@@ -9,14 +11,39 @@ If you get confused at any time above is a link to the MDN doc on Javascript Var
 // Declare a variable
 // Refer to the link above
 
+var x;
+
+var hello;                       //short names that make sense are best
+
+var stuffBeingDoneSlightlyRight; //long variable names aren't suggested but are possible
+
 // Store a string in a variable
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+var x = 'hello world'; 
+
+var stuff = "this is also a string"   // notice there are different ways to make a string
+
+var lot = 'you can put anything in a string'
 
 //Store a Number in a variable
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
+var bob = 5;
+
+var num1 = 2;
+
+var num5 = 0; 
+
 // Store a Boolean value in a variable
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
 
+var cat = false;
+var dog = true;
+
 // console.log  your answers
 //https://developer.mozilla.org/en-US/docs/Web/API/Console/log
+
+console.log(x);
+console.log(bob);
+console.log(cat);
