22bf481161bfc07da1a7bd6959837b07cee5756f
Aaron Endsley <aaronendsley@gmail.com>
2017-08-04 21:42:44 -0500

    added variables lesson 2

---
+++ variables-2/variables-2.js
@@ -0,0 +1,24 @@
+///get data from a prompt that is stored in a variable
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt4
+
+// console.log your variable that is storing your data from your prompt
+//https://developer.mozilla.org/en-US/docs/Web/API/Console/log
+
+// Make your variables data appear in an alert
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+// create two variables with two different numbers stored in them  create a new variable and add the other two
+// variables inside it, console.log Your solution
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+
+// create two variables with two different numbers, create a third variable and subract the other two variables inside it.
+// console.log your solution
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+
+// create two variables with two different numbers, create a third variable and multiply the other two variables inside it.
+// console.log your solution
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+
+// create two variables with two different numbers, create a third variable and division the other two variables inside it.
+// console.log your solution
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
